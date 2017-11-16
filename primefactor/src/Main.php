00edffa808399f9e3ed04d4afc9bce8c3d573766
shobhit.verma <shobhit.verma@webvirtue.com>
2017-11-16 15:48:21 +0530

    update

--- primefactor/src/Main.php
+++ primefactor/src/Main.php
@@ -6,7 +6,14 @@ namespace Demo\PrimeFactor;
 
 class Main
 {
-    public function calculatePrimeFactor(){
-        return 1;
+    public function calculatePrimeFactorFor(int $n): array
+    {
+        $primeFactors = array();
+        for ($counter = 2; $n > 1; $counter++) {
+            for (; $n % $counter == 0; $n /= $counter) {
+                array_push($primeFactors, $counter);
+            }
+        }
+        return $primeFactors;
     }
 }
